9504928f9f58ebd3f112839cb147f22eb8535542
Felipe Akira <feakinoz@gmail.com>
2023-09-10 15:16:10 -0300

    Implementação botão pausar tabuleiro pequeno

---
+++ js/jogo.js
@@ -0,0 +1,7 @@
+function pausarJogo() {
+    document.getElementById("fundo-pausa").style.display = "block";
+}
+
+function voltarJogo() {
+    document.getElementById("fundo-pausa").style.display = "none";
+}
