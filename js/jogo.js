b8b937bc43649a964f69e9d90c5ac9d53f1f1ac1
felipeakira-exe <feakinoz@gmail.com>
2023-09-19 12:10:28 -0300

    Criação do tabuleiro

--- js/jogo.js
+++ js/jogo.js
@@ -5,3 +5,28 @@ function pausarJogo() {
 function voltarJogo() {
     document.getElementById("fundo-pausa").style.display = "none";
 }
+
+
+/* JOGO */
+const NUM_COLUNAS = 10;
+const NUM_LINHAS = 20;
+
+const tabuleiro = document.getElementById('tabuleiro');
+const matriz = [];
+
+for (let i = 0; i < NUM_LINHAS; i++) {
+    for(let j = 0; j < NUM_COLUNAS; j++) {
+        const celula_fundo = document.createElement('div');
+        celula_fundo.classList.add('celula_fundo');
+        tabuleiro.appendChild(celula_fundo);
+    }
+    const linha = Array(NUM_COLUNAS).fill(0);
+    matriz.push(linha);
+}
+
+console.log(matriz);
+
+botaoIniciar = document.getElementById('btn-iniciar');
+botaoIniciar.addEventListener('click', (event) => {
+    
+})
